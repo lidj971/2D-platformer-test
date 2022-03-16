d8902f9927865d9c6e90a5467364146add57bc2c
lidj971 <lidj971@outlook.fr>
2022-03-16 17:33:59 -0400

    Ajout de fonction de zoom/dezoom a la camera

--- 2D Platformer Test/Assets/Scripts/Camera/MultipleTargetCamera.cs
+++ 2D Platformer Test/Assets/Scripts/Camera/MultipleTargetCamera.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Camera))]
 public class MultipleTargetCamera : MonoBehaviour
 {
     List<Transform> targets;
@@ -12,16 +13,48 @@ public class MultipleTargetCamera : MonoBehaviour
 
     public float smoothTime;
 
+    public float minZoom = 10f;
+    public float maxZoom = 40f;
+    public float zoomLimiter = 50f;
+
+    private Camera cam;
+
+    private void Start()
+    {
+        cam = GetComponent<Camera>();
+    }
+
     // Update is called once per frame
     void LateUpdate()
     {        
         if (targets.Count == 0) return;
-        
+
+        Move();
+        Zoom();
+    }
+
+    private void Move()
+    {
         Vector3 centerPoint = GetCenterPoint();
-
         Vector3 newPosition = centerPoint + offset;
-
         transform.position = Vector3.SmoothDamp(transform.position, newPosition, ref velocity, smoothTime);
+    }
+
+    void Zoom()
+    {
+        float newZoom = Mathf.Lerp(minZoom, maxZoom, GetGreatestDistance() / zoomLimiter);
+        cam.fieldOfView = Mathf.Lerp(cam.fieldOfView, newZoom, Time.deltaTime);
+    }
+
+    float GetGreatestDistance()
+    {
+        var bounds = new Bounds(targets[0].position, Vector3.zero);
+        for (int i = 0; i < targets.Count; i++)
+        {
+            bounds.Encapsulate(targets[i].position);
+        }
+
+        return bounds.size.x;
     }
 
     Vector3 GetCenterPoint()
